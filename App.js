f14a5b26600216d936a189bd5161d213a1663a62
z1gonzo <goonsky@gmail.com>
2022-08-29 20:49:01 +0200

    base navigation

--- App.js
+++ App.js
@@ -1,12 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+import HomeScreen from './screens/HomeScreen';
+import MapScreen from './screens/MapScreen';
+import { store } from './store';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator()
 
 export default function App() {
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <NavigationContainer>
+        <SafeAreaProvider>
+          <Stack.Navigator>
+            <Stack.Screen
+              name="HomeScreen"
+              component={HomeScreen}
+              options={{
+                headerShown: false
+              }}
+            />
+            <Stack.Screen
+              name="MapScreen"
+              component={MapScreen}
+              options={{
+                headerShown: false
+              }}
+            />
+          </Stack.Navigator>
+
+          {/* <HomeScreen /> */}
+
+
+        </SafeAreaProvider>
+      </NavigationContainer>
+    </Provider>
   );
 }
 
